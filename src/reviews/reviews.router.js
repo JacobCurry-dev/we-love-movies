e0077609687abf48d6ecba3d546aa84b07bab651
Jacob Curry <jcurr0708@gmail.com>
2022-08-01 16:43:07 -0400

    Added file structure for routers/conts/services

---
+++ src/reviews/reviews.router.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const controller = require("./reviews.controller");
+
+
+
+
+module.exports = router;
---
+++ src/theaters/theaters.router.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const controller = require("./theaters.controller");
+
+
+
+
+module.exports = router;
