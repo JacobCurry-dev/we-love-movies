c620e6ed3dd5b4bbe60f1d78fa551d2682d3a83c
JB Burroughs <jbburroughs.dev@gmail.com>
2022-08-02 11:11:46 -0400

    read single movie route built

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,30 +1,42 @@
 const moviesService = require("./movies.service");
 
+const movieExists = (req, res, next) => {
+    moviesService
+      .read(req.params.movieId)
+      .then((movie) => {
+        if (movie) {
+          res.locals.movie = movie;
+          return next();
+        }
+        next({ status: 404, message: `Movie cannot be found.` });
+      })
+      .catch(next);
+};
+
 const list = (req, res, next) => {
-    
-    moviesService   
-        .list()
-        .then((data) => res.json({ data }))
-        .catch(next);
-}
+  moviesService
+    .list()
+    .then((data) => res.json({ data }))
+    .catch(next);
+};
 
 const listIfShowing = (req, res, next) => {
-    if (req.query.is_showing) {
-        moviesService
-            .listIfShowing()
-            .then((data) => res.json({ data }))
-            .catch(next);
-    }
-    next()
-}
+  if (req.query.is_showing) {
+    moviesService
+      .listIfShowing()
+      .then((data) => res.json({ data }))
+      .catch(next);
+  }
+  next();
+};
 
 const read = (req, res, next) => {
-    const { movie: data } = res.locals;
-    res.json({ data });
-}
+  const { movie: data } = res.locals;
+  res.json({ data });
+};
 
 module.exports = {
-    list,
-    read,
-    listIfShowing
+  list,
+  read: [movieExists, read],
+  listIfShowing,
 };
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,24 +1,23 @@
 const knex = require("../db/connection");
 
 const list = () => {
-    return knex("movies").select("*");
-}
+  return knex("movies").select("*");
+};
 
 const listIfShowing = () => {
-    return knex("movies as m")
-        .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
-        .select("m.*")
-        .where({ "mt.is_showing": true })
-        .groupBy("m.movie_id")
-}
+  return knex("movies as m")
+    .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+    .select("m.*")
+    .where({ "mt.is_showing": true })
+    .groupBy("m.movie_id");
+};
 
 const read = (movieId) => {
-    return knex("movies").select("*").where({ movie_id: movieId }).first();
-}
-
+  return knex("movies").select("*").where({ movie_id: movieId }).first();
+};
 
 module.exports = {
-    list,
-    listIfShowing,
-    read,
+  list,
+  listIfShowing,
+  read,
 };
